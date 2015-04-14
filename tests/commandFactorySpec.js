b770c69b7d22c34da40fea71911e29adb19fe590
JakenHerman <jch053@shsu.edu>
2015-04-14 02:15:48 +0000

    s

--- tests/commandFactorySpec.js
+++ tests/commandFactorySpec.js
@@ -10,13 +10,13 @@ describe("Command Factory", function() {
 	});
 
 	it("Should return a default command if it can't find one", function() {
-		var cmd = cmdFact.getCommand(["deleteman123", "test msg"]);
+		var cmd = cmdFact.getCommand(["ImJaken", "test msg"]);
 		cmd.should.be.instanceOf(commands.send);
 	});
 
 	it("Should pass all the required parameters to the command constructor", function() {
-		var cmd = cmdFact.getCommand(["deleteman123", "test msg"]);
-		cmd.destination.should.eql("deleteman123");
+		var cmd = cmdFact.getCommand(["ImJaken", "test msg"]);
+		cmd.destination.should.eql("ImJaken");
 		cmd.message.should.eql("test msg");	
 	});
 });
